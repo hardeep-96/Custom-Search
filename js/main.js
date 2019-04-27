96d37b800d030c7462459ba64e6df5099ef01f92
= <=>
2019-04-28 03:43:59 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const search = document.querySelector("#search");
+const matchList = document.querySelector("#match-list");
+
+const searchStates = async searchText => {
+  const res = await fetch("../data/states.json");
+  const states = await res.json();
+
+  let matches = states.filter(state => {
+    const reg = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(reg) || state.abbr.match(reg);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = "";
+  }
+
+  outputhtml(matches);
+  //   console.log(matches);
+};
+
+const outputhtml = matches => {
+  if (matches.length > 0) {
+    let html = matches
+      .map(
+        match => `
+        <div class="card card-body mb-1">
+        <h4>${match.name} ${match.abbr} <span class="text-primary">${
+          match.capital
+        }</span></h4>
+        <small>Lat: ${match.lat}/ Long: ${match.long}</small>
+        </div>`
+      )
+      .join("");
+
+    matchList.innerHTML = html;
+  }
+};
+
+search.addEventListener("input", () => searchStates(search.value));
